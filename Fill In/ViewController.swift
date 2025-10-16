d270284a2b37322bfbdd39ee7e50967e42fee852
Burdakovskiy <www.deluxesss@gmail.com>
2025-10-16 12:00:07 +0200

    Initial Commit

---
+++ Fill In/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fill In
+//
+//  Created by Дмитрий on 16.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
